63e62b6256fa7d53d2a978f516f0ca7f08121a91
siftal <m.khanmohamadzadeh@gmail.com>
2021-12-17 12:48:13 -0500

    remove 'DollarForEveryone' item from the select

--- js/main.js
+++ js/main.js
@@ -549,7 +549,7 @@ $(document).ready(function () {
       return;
     }
     const id = val.trim();
-    if (["BrightID", "Bitu", "SeedConnected", "DollarForEveryone", "SocialRecoverySetup"].includes(id)) {
+    if (["BrightID", "Bitu", "SeedConnected", "SocialRecoverySetup"].includes(id)) {
       selectVerification(id);
     } else if (allNodes[id]) {
       if (graphNodes[id]) {
