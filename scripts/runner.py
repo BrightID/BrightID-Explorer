a5885bfedd1654fb703c98125a932bb6172af56d
siftal <m.khanmohamadzadeh@gmail.com>
2022-03-07 09:01:32 -0800

    fix an issue

--- scripts/runner.py
+++ scripts/runner.py
@@ -7,7 +7,7 @@ import json_generator
 def main():
     t1 = time.time()
     clustering.main()
-    # bitu.main()
+    bitu.main()
     json_generator.main()
     print(f'finished in {int(time.time() - t1)} seconds\t({time.ctime()})')
 
