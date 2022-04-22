1b9589d6c03a6b82d8a35f889cb0a7db48f88fb1
siftal <m.khanmohamadzadeh@gmail.com>
2022-04-22 01:49:18 -0700

    update runner.py

--- scripts/runner.py
+++ scripts/runner.py
@@ -1,15 +1,28 @@
+import os
 import time
-import bitu
-import clustering
+import utils
+import bitu_updater
 import json_generator
 
 
 def main():
-    t1 = time.time()
-    clustering.main()
-    bitu.main()
-    json_generator.main()
-    print(f'finished in {int(time.time() - t1)} seconds\t({time.ctime()})')
+    start_time = time.time()
+
+    # gets and restores database backup
+    utils.restore_backup()
+
+    # updates nodes' position
+    print('\nUpdating positions...')
+    os.system('node ./position_updater/runner.js')
+
+    # calculates bitu score
+    bitu_updater.eligible_finder.run()
+    bitu_updater.score_calculator.run()
+
+    # updates BrightID explorer json file
+    json_generator.run()
+
+    print(f'finished in {int(time.time() - start_time)} seconds\t({time.ctime()})')
 
 
 if __name__ == '__main__':
