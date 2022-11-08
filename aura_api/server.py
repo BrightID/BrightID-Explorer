bf4dd5007c6c79dd96a2b9ceaebdd839aeeada18
Siftal <m.khanmohamadzadeh@gmail.com>
2022-11-08 09:54:03 -0500

    check requests by marshmallow & add the reply feature.

--- aura_api/server.py
+++ aura_api/server.py
@@ -1,3 +1,4 @@
+from marshmallow import Schema, fields, ValidationError
 from flask import Flask, request
 from arango import ArangoClient
 import ed25519
@@ -11,10 +12,33 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 
+class AddCommentSchema(Schema):
+    user = fields.String(required=True)
+    comment = fields.String(required=True)
+    nodes = fields.List(fields.String(), metadata={'allow_blank': False})
+    aura = fields.String(required=True)
+    signing_key = fields.String(required=True)
+    sig = fields.String(required=True)
+    category = fields.String(required=True)
+    mainCommentKey = fields.String(required=True)
+
+
+class RemoveCommentSchema(Schema):
+    _key = fields.Integer(required=True)
+    user = fields.String(required=True)
+    aura = fields.String(required=True)
+    signing_key = fields.String(required=True)
+    sig = fields.String(required=True)
+
+
+add_comment_schema = AddCommentSchema()
+remove_comment_schema = RemoveCommentSchema()
+
+
 @app.route('/comment', methods=['PUT', 'DELETE'])
 def comments():
     data = request.get_json()
-    print(data)
+    print("DATA", data)
     if request.method == 'PUT':
         return add_comment(data)
     elif request.method == 'DELETE':
@@ -22,16 +46,18 @@ def comments():
 
 
 def add_comment(data):
-    for key in ['user', 'comment', 'nodes', 'aura', 'signing_key', 'sig']:
-        if key not in data:
-            raise ErrorToClient(f'Missing {key} value', 400)
-    signing_key = data['signing_key']
+    try:
+        data = add_comment_schema.load(data, partial=("mainCommentKey",))
+    except ValidationError as err:
+        raise ErrorToClient(err.messages, 400)
 
+    signing_key = data['signing_key']
     verify_sig(data)
 
     aura_node = config.AURA_NODE if data['aura'] == 'aura' else config.AURA_TEST_NODE
-    snapshot_db = ArangoClient(hosts=aura_node).db('snapshot')
     db = ArangoClient(hosts=aura_node).db('_system')
+    snapshot_db = ArangoClient(hosts=aura_node).db('snapshot')
+
     c = db.aql.execute('''
         FOR e IN users
             FILTER e._key == @user
@@ -54,34 +80,47 @@ def add_comment(data):
     if energy == 0:
         raise ErrorToClient("User doesn't have energy", 403)
 
-    comments_coll = db.collection('comments')
-    metadata = comments_coll.insert({
+    if data.get('mainCommentKey'):
+        if not db['comments'].has(str(data['mainCommentKey'])):
+            raise ErrorToClient('The main comment not found', 403)
+
+    metadata = db['comments'].insert({
         'user': data['user'],
         'comment': data['comment'],
+        'category': data['category'],
         'nodes': data['nodes'],
+        'mainCommentKey': data.get('mainCommentKey', ''),
         'timestamp': int(time.time() * 1000),
     })
     return json.dumps({'status': 200, '_key': metadata['_key']})
 
 
 def remove_comment(data):
-    for key in ['user', '_key', 'aura', 'signing_key', 'sig']:
-        if key not in data:
-            raise ErrorToClient(f'Missing {key} value', 400)
+    try:
+        data = remove_comment_schema.load(data)
+    except ValidationError as err:
+        raise ErrorToClient(err.messages, 400)
 
     verify_sig(data)
 
     aura_node = config.AURA_NODE if data['aura'] == 'aura' else config.AURA_TEST_NODE
     db = ArangoClient(hosts=aura_node).db('_system')
-    comments_coll = db.collection('comments')
-    comment = comments_coll.get(str(data['_key']))
-    if (not comment):
+
+    comment = db['comments'].get(str(data['_key']))
+    if not comment:
         raise ErrorToClient('The comment not found', 404)
 
-    if (comment.get('user') != data['user']):
+    if comment.get('user') != data['user']:
         raise ErrorToClient('Only the comment writer can remove it', 403)
 
-    comments_coll.delete(str(data['_key']))
+    db.aql.execute('''
+        for c in comments
+            filter c._key == @key
+            or c.mainCommentKey == @key
+            REMOVE { _key: c._key } IN comments
+    ''', bind_vars={
+        'key': data['_key']
+    })
     return json.dumps({'status': 200})
 
 
@@ -91,11 +130,12 @@ def verify_sig(data):
     verifying_key = ed25519.VerifyingKey(hex_signing_key, encoding='hex')
     del data['sig']
     del data['signing_key']
-    message = json.dumps(data, separators=(',', ':')).encode('ascii')
+    message = json.dumps(data, sort_keys=True,
+                         separators=(',', ':')).encode('ascii')
     try:
         verifying_key.verify(sig, message, encoding='hex')
         print('Signature is verified')
-    except Exception as e:
+    except:
         raise ErrorToClient('Bad signature', 401)
 
 
